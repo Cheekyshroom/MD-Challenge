38d7be008278f96ff5cec61cab371b32c29d8afe
Peter Herniman <phernima@ucsc.edu>
2016-03-01 22:39:27 -0800

    Uses GET and POST requests internally properly

--- downloader.rb
+++ downloader.rb
@@ -3,6 +3,26 @@ require 'net/http'
 require 'socket'
 
 class DownloadQueue
+  #could've used a struct instead
+  class Connection
+    #holds information about a download thread so we can list it nicely
+    attr_accessor :thread, :url
+    def initialize(thread, url)
+      @thread = thread
+      @url = url
+    end
+    def status
+      status = @thread.status 
+      if status == false #false indicates success
+        return "Done", @thread.value
+      elsif status == nil #nil indicates failure
+        return "Failure", status
+      else
+        return "In progress", status
+      end
+    end
+  end
+
   def initialize()
     @elements = []
   end
@@ -10,9 +30,10 @@ class DownloadQueue
   def add(url)
     #instead of having a download queue, just pop off another thread for each
     #download, so they all get done at once and hopefully faster
-    @elements << Thread.new do
+    thread = Thread.new do
       Net::HTTP.get(URI(url))
     end
+    @elements << Connection.new(thread, url)
     @elements.length-1 #return our job id
   end
 
@@ -22,14 +43,16 @@ class DownloadQueue
     if i < 0 or i >= @elements.length
       return "ID out of range."
     end
-    status = @elements[i].status 
-    if status == false #false indicates success
-      return "Done", @elements[i].value
-    elsif status == nil #nil indicates failure
-      return "Failure", status
-    else
-      return "In progress.", status
+    return @elements[i].status
+  end
+  def list
+    #did this with reduce but realized I needed the index to have a nice list
+    out = ""
+    @elements.length.times do |i|
+      status, _ = @elements[i].status
+      out << "#{i} [#{status}]: #{@elements[i].url}\n"
     end
+    out + "Done\n"
   end
 end
 
@@ -53,14 +76,26 @@ class Server
   def handle_request
     client = @server.accept
     begin
-      request = client.gets.chomp
+      string = client.gets.lstrip.chomp
+      endOfRequest = string.index(' ')
+      #make sure that the request is at least two words so substringing won't break
+      if endOfRequest == nil
+        puts("INVALID REQUEST '#{string}'")
+        client.puts("INVALID REQUEST '#{string}'")
+        return true
+      end
+      #split our string into a GET/POST and an argument
+      request = string[0..endOfRequest-1]
+      rest = string[endOfRequest+1..string.length]
       case request
-        when "kill"
-          return false
-        when "check"
+        when "GET" #GETS only query or list
+          #if they query a wildcard, give them a list of downloads back
+          if rest == "*"
+            client.puts(@queue.list)
+            return true
+          end
           #query our queue to see if our download has finished
-          id = client.gets
-          status, value = @queue.query(id)
+          status, value = @queue.query(rest)
           #print the status regardless of what it is
           client.puts(status)
           if status == "Done"
@@ -69,13 +104,16 @@ class Server
             client.puts(size)
             client.write(value)
           end
-        when "download"
+        when "POST" #POSTS can kill our server or start a new download
+          if rest == "kill"
+            return false #kill our server if it's a kill request, otherwise handle
+          end
           #add a download to our queue
-          uri = client.gets
+          uri = rest
           i = @queue.add(uri)
           client.puts(i)
         else
-          puts("INVALID REQUEST #{request}")
+          puts("INVALID REQUEST '#{string}' '#{request}' '#{rest}'")
       end
     ensure
       client.close
@@ -91,7 +129,7 @@ class Client
       case ARGV[0]
         when "kill"
           #send a kill message to our daemon
-          @socket.puts("kill")
+          @socket.puts("POST kill")
         when "check"
           #check the progress of a job id or get the resulting webpage
           check_id(ARGV[1])
@@ -106,9 +144,22 @@ class Client
   end
 
   def check_id(id)
+    #if we want to get a list:
+    if id == "all"
+      #print output until we get "Done"
+      @socket.puts("GET *")
+      loop do
+        line = @socket.gets.chomp
+        if line == "Done"
+          break
+        end
+        puts(line)
+      end
+      return
+    end
+    #otherwise
     #send a check message and get the download status back
-    @socket.puts("check")
-    @socket.puts(id)
+    @socket.puts("GET #{id}")
     status = @socket.gets.chomp
     if status == "Done"
       #if our download's done, print it to stdout
@@ -122,12 +173,11 @@ class Client
   end
 
   def download_url(s)
-    #make our url begin with http:// or https:// otherwise Net::HTTP
-    #will complain
+    #make our url begin with http:// or https:// otherwise Net::HTTP will
+    #complain
     url = (s[0..6] == "http://" or s[0..7] == "https://") ? s : "http://"+s
     #send our download message and the url
-    @socket.puts("download")
-    @socket.puts(url)
+    @socket.puts("POST #{url}")
     #and print the id we get back
     id = @socket.gets
     puts(id)
@@ -135,7 +185,7 @@ class Client
 end
 
 if ARGV.length < 1
-  puts("Usage: downloader [start] [kill] [check id] [download url]")
+  puts("Usage: downloader [start] [kill] [check [id|all]] [url]")
   exit
 end
 if ARGV[0] == "start"
